8da0a8e1a980d6107e641917446477a99cb94719
Grant Taylor <grant.i.taylor@student.uts.edu.au>
2026-05-11 22:54:54 +1000

    ohh yeah

--- src/train.py
+++ src/train.py
@@ -2,27 +2,37 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-OBSTACLE_PENALTY = -25 # big penalty for hitting obstacle
+FALL_PENALTY = -250 # big penalty for hitting obstacle
 GOAL_REWARD = 100    # big reward for reaching goal
-STEP_PENALTY = -0.05 # Penalises more steps
-PROGRESS_REWARD_SCALE = 10.1 # multiples negative or positive reward for movement
-MINIMUM_SAFE_DISTANCE = 1.0
+STEP_PENALTY = -0.1 # Penalises more steps
+PROGRESS_REWARD_SCALE = 5 # multiples negative or positive reward for movement
+STILL_ALIVE_REWARD = 0
 
 class BipedalRewardWrapper(gym.RewardWrapper):  # ← inherit from gymnasium
+
     def reward(self, reward):
-        if reward <= -100:
-            return -1.0
         return reward
     
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        
-        # Now you can use obs, action, AND reward together
-        forward_velocity = obs[2]  # BipedalWalker specific
-        if reward <= -100:
-            reward = -1.0
+
+        reward += STEP_PENALTY
+
+        forward_velocity = obs[2]   # Hoizontal velocity in observation space
+
+        reward += forward_velocity * PROGRESS_REWARD_SCALE  # reward speed
+        if forward_velocity < 0:
+            reward += forward_velocity * PROGRESS_REWARD_SCALE  # Scale punish negative movement
         else:
-            reward += forward_velocity * PROGRESS_REWARD_SCALE  # reward speed
+            reward += forward_velocity  # Reward positive horizontal velocity
+
+        if terminated == True:
+            reward += FALL_PENALTY # if fall big penalty
+        else:
+            reward += STILL_ALIVE_REWARD    # currenlty set to 0
+
+        if truncated == True:
+            reward += GOAL_REWARD
 
         return obs, reward, terminated, truncated, info
 
@@ -46,7 +56,7 @@ model = PPO(
     n_steps=2048,      # steps per env before each update
     batch_size=64,
     learning_rate=3e-4,
-    ent_coef=0.005,    # small entropy bonus encourages exploration
+    ent_coef=0.05,    # small entropy bonus encourages exploration
     verbose=1
 )
 
