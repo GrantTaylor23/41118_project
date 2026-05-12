400deb0f5c3ffabaf0a3d828c8b33857cc372835
Grant Taylor <grant.i.taylor@student.uts.edu.au>
2026-05-12 11:33:41 +1000

    Added hardcore world argument

--- src/watch.py
+++ src/watch.py
@@ -2,7 +2,7 @@ from stable_baselines3 import PPO
 import gymnasium as gym
 
 model = PPO.load("../model/bipedal_ppo")
-env = gym.make("BipedalWalker-v3", render_mode="human")
+env = gym.make("BipedalWalker-v3", hardcore=True, render_mode="human") # If hardcore is on here the model will be place in hardcore world
 
 obs, _ = env.reset()
 for _ in range(2000):
