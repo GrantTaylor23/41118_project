1e572e757ac106e48461fa3ae2f5dda5f39a5e08
Grant Taylor <grant.i.taylor@student.uts.edu.au>
2026-05-12 11:33:10 +1000

    Removed incorrect trucated fuctionality. Added stationary penalty

--- src/train.py
+++ src/train.py
@@ -7,12 +7,11 @@ GOAL_REWARD = 100    # big reward for reaching goal
 STEP_PENALTY = -0.1 # Penalises more steps
 PROGRESS_REWARD_SCALE = 5 # multiples negative or positive reward for movement
 STILL_ALIVE_REWARD = 0
-TOTAL_TIMESTEPS = 100_000
+TOTAL_TIMESTEPS = 500_000
+STATIONARY_PENALTY = -5
 
 class BipedalRewardWrapper(gym.RewardWrapper):  # ← inherit from gymnasium
 
-    def reward(self, reward):
-        return reward
     
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
@@ -20,25 +19,25 @@ class BipedalRewardWrapper(gym.RewardWrapper):  # ← inherit from gymnasium
         reward += STEP_PENALTY
 
         forward_velocity = obs[2]   # Hoizontal velocity in observation space
+        
+        if forward_velocity == 0:
+            reward += STATIONARY_PENALTY
 
-        reward += forward_velocity * PROGRESS_REWARD_SCALE  # reward speed
         if forward_velocity < 0:
             reward += forward_velocity * PROGRESS_REWARD_SCALE  # Scale punish negative movement
         else:
-            reward += forward_velocity  # Reward positive horizontal velocity
+            reward += forward_velocity * PROGRESS_REWARD_SCALE  # Reward positive horizontal velocity
 
         if terminated == True:
             reward += FALL_PENALTY # if fall big penalty
         else:
             reward += STILL_ALIVE_REWARD    # currenlty set to 0
 
-        if truncated == True:
-            reward += GOAL_REWARD
 
         return obs, reward, terminated, truncated, info
 
 def make_env():
-    return BipedalRewardWrapper(gym.make("BipedalWalker-v3", render_mode="rgb_array"))
+    return BipedalRewardWrapper(gym.make("BipedalWalker-v3", hardcore=True, render_mode="rgb_array"))
 
 env = make_vec_env(make_env, n_envs=4)
 
@@ -53,6 +52,6 @@ model = PPO(
     tensorboard_log="./ppo_tensorboard/"
 )
 
-model = PPO.load("../model/bipedal_ppo", env=env)
+# model = PPO.load("../model/bipedal_ppo", env=env)
 model.learn(total_timesteps=TOTAL_TIMESTEPS)
 model.save("../model/bipedal_ppo")
